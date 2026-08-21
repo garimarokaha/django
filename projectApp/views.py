24012f758f039a24fc3bb710c58949cf5b760c3f
garimarokaha <garimarokaha5@gmail.com>
2026-08-21 16:54:37 +0545

    urls.py is done

--- projectApp/views.py
+++ projectApp/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>Welcome to My Django Website</h1>")
+def  about(request):
+    return HttpResponse("<h1>About Page</h1><p>This is the about page of the website.</p>")
