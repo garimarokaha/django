ec716dc2450c5f2b90e167aaa5c19f48d362f4c5
garimarokaha <garimarokaha5@gmail.com>
2026-08-23 17:08:08 +0545

    done with templates

--- projectApp/views.py
+++ projectApp/views.py
@@ -1,9 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
 def home(request):
-    return HttpResponse("<h1>Welcome to My Django Website</h1>")
-def  about(request):
-    return HttpResponse("<h1>About Page</h1><p>This is the about page of the website.</p>")
+    return render(request, 'projectApp/home.html')
